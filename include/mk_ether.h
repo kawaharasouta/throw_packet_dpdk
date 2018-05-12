31e4f9e1c92ffcca4621353b313604e7d2aef02c
Kawaharasouta <kawahara6514@gmail.com>
2018-05-12 20:52:31 +0900

    rm

--- include/mk_ether.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MK_ETHER_H
-#define __MK_ETHER_H
-
-#include<sys/types.h>
-//#include"knet/ethernet.h"
-
-extern void mk_ether(struct ether_hdr *hdr, u_int8_t *d_addr, u_int8_t *s_addr);
-
-#endif
